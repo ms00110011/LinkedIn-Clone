c1a6b998c8125abdec06713e0b1574fe4e782444
ms00110011 <ms00110011@gmail.com>
2022-08-25 17:28:39 +0530

    Project started and made the navbar

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { Header } from './Components/Header';
+
+function App() {
+  return (
+    <div className="app">
+
+    <Header />
+
+
+
+    </div>
+  );
+}
+
+export default App;
