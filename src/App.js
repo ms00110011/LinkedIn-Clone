638ebad3e789ea2648ea8ed0a758ed2133b63bd3
ms00110011 <ms00110011@gmail.com>
2022-08-30 22:54:10 +0530

    Sidebar added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 import { Header } from './Components/Header';
+import { Sidebar } from './Components/Sidebar';
 
 function App() {
   return (
     <div className="app">
 
     <Header />
+
+    <div className="appbody">
+
+    <Sidebar />
+
+    </div>
 
 
 
