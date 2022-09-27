825c11ef94a4a58448a4541a3d972274b8d05214
ms00110011 <ms00110011@gmail.com>
2022-09-27 18:52:16 +0530

    Created Login Page

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import "./App.css";
 import { Feed } from "./Components/Feed";
 import { Header } from "./Components/Header";
 import { Sidebar } from "./Components/Sidebar";
+import { selectUser } from "./features/userSlice";
+import { useSelector } from "react-redux";
+import { Login } from "./Components/Login";
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
       <Header />
 
-      <div className="appbody">
-        <Sidebar />
-        <Feed />
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="appbody">
+          <Sidebar />
+          <Feed />
+        </div>
+      )}
     </div>
   );
 }
